664c76108a910e9a8c4c30450ccfac6866f592fb
Maksim Gusev <51376126+AlanTheKnight@users.noreply.github.com>
2024-04-02 16:47:15 +0300

    Create project structure

---
+++ lab6/apps/common/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+}
+
+group = "alantheknight.lab6"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:32.1.1-jre")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
---
+++ lab6/apps/server/src/main/java/alantheknight/lab6/server/Main.java
@@ -0,0 +1,7 @@
+package alantheknight.lab6.server;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
